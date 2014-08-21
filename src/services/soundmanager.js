f9bdbde8f18f6fd7e0c87cf93be42b9965bb02c4
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2014-08-22 00:23:47 +0300

    Play/pause active track

--- src/services/soundmanager.js
+++ src/services/soundmanager.js
@@ -1,25 +1,46 @@
 var sm = require('sound-manager');
+var isReady = false;
 
 module.exports = function (appstate, type, data) {
-  if (type === 'toggle:play') {
-    var activeTrack = appstate.get('activeTrack');
-
-    if (data.track.get('id') !== activeTrack.get('id')) {
-      return appstate.set('activeTrack', data.track.set('isPlaying', true));
-    }
-
-    return appstate.set('activeTrack', activeTrack.set('isPlaying', !activeTrack.get('isPlaying')));
-  }
-
   if (type === 'app:start') {
     sm.setup({
       url: 'swf',
       flashVersion: 9,
       preferFlash: false,
       onready: function() {
-        console.log('sound-manager is ready');
+        isReady = true;
       }
     });
+
+    return appstate;
+  }
+
+  // don't do anything while sound manager is initializing
+  if (!isReady) {
+    return appstate;
+  }
+
+  if (type === 'toggle:play') {
+    var activeTrack = appstate.get('activeTrack');
+
+    if (data.track.get('id') !== activeTrack.get('id')) {
+      sm.stop(activeTrack.get('id'));
+      sm.unload(activeTrack.get('id'));
+
+      sm.createSound({
+        id: data.track.get('id'),
+        url: data.track.get('url'),
+        autoLoad: true,
+        autoPlay: true,
+        volume: 100
+      });
+
+      return appstate.set('activeTrack', data.track.set('isPlaying', true));
+    }
+
+    sm.togglePause(activeTrack.get('id'));
+
+    return appstate.set('activeTrack', activeTrack.set('isPlaying', !activeTrack.get('isPlaying')));
   }
 
   return appstate;
