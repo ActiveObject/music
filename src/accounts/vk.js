9a4aaee54983d779d457b8c02a0546f50711f673
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2014-09-24 23:30:09 +0300

    Using webpack define plugin to set credentials in the code

--- src/accounts/vk.js
+++ src/accounts/vk.js
@@ -1,7 +1,7 @@
 exports.API_BASE = 'https://api.vk.com/method';
 exports.AUTH_URL = 'https://oauth.vk.com/authorize';
-exports.REDIRECT_URI = process.env.MUSIC_APP_HOST;
+exports.REDIRECT_URI = __MUSIC_APP_HOST__;
 exports.API_VERSION = '5.23';
-exports.APP_ID = process.env.MUSIC_APP_ID;
+exports.APP_ID = __MUSIC_APP_ID__;
 exports.PERMISSIONS = ['friends', 'audio', 'groups', 'wall', 'offline', 'newsfeed'];
 exports.DISPLAY = 'popup';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 var path = require('path');
 var webpack = require('webpack');
 
+if (!Number(process.env.MUSIC_APP_ID)) {
+  throw new Error('MUSIC_APP_ID env var should be a number');
+}
+
+if (!process.env.MUSIC_APP_HOST) {
+  throw new Error('MUSIC_APP_HOST env var should be a valid hostname');
+}
+
+var definePlugin = new webpack.DefinePlugin({
+  __MUSIC_APP_HOST__: JSON.stringify(process.env.MUSIC_APP_HOST),
+  __MUSIC_APP_ID__: JSON.stringify(process.env.MUSIC_APP_ID)
+});
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -17,6 +30,7 @@ module.exports = {
   },
   watchDelay: 200,
   plugins: [
+    definePlugin,
     new webpack.IgnorePlugin(/vertx/)
   ]
 };
