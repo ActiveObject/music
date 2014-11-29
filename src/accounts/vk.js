494842e2ea84995a2f8873bf7a1428ff183b345a
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2014-11-29 21:13:27 +0200

    Use the current location as REDIRECT_URI

--- src/accounts/vk.js
+++ src/accounts/vk.js
@@ -1,6 +1,6 @@
 exports.API_BASE = 'https://api.vk.com/method';
 exports.AUTH_URL = 'https://oauth.vk.com/authorize';
-exports.REDIRECT_URI = process.env.MUSIC_APP_HOST;
+exports.REDIRECT_URI = window.location.origin;
 exports.API_VERSION = '5.23';
 exports.APP_ID = process.env.MUSIC_APP_ID;
 exports.PERMISSIONS = ['friends', 'audio', 'groups', 'wall', 'offline', 'newsfeed'];
--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,9 @@ if (!Number(process.env.MUSIC_APP_ID)) {
   throw new Error('MUSIC_APP_ID env var should be a number');
 }
 
-if (!process.env.MUSIC_APP_HOST) {
-  throw new Error('MUSIC_APP_HOST env var should be a valid hostname');
-}
-
 var definePlugin = new webpack.DefinePlugin({
   'process.env': {
     NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-    MUSIC_APP_HOST: JSON.stringify(process.env.MUSIC_APP_HOST),
     MUSIC_APP_ID: JSON.stringify(process.env.MUSIC_APP_ID)
   }
 });
