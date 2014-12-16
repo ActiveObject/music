faebcdc51229478863d025698487f662acb95501
Vasyl Pryimachuk <vasyl.priymachuk@sethq.com>
2014-12-16 15:48:15 +0200

    Cleanup track value

--- src/values/track.js
+++ src/values/track.js
@@ -2,10 +2,6 @@ var _ = require('underscore');
 var attrEquals = require('app/utils').attrEquals;
 
 function Track(attrs) {
-  if (!(this instanceof Track)) {
-    return new Track(attrs);
-  }
-
   this.id = Number(attrs.id);
   this.artist = attrs.artist;
   this.title = attrs.title;
@@ -15,18 +11,6 @@ function Track(attrs) {
   this.owner_id = attrs.owner_id;
   this.url = attrs.url;
 }
-
-Track.fromEntity = function (entity) {
-  return new Track({
-    id: entity[':track/vkid'],
-    artist: entity[':track/artist'],
-    title: entity[':track/title'],
-    duration: entity[':track/duration'],
-    owner_id: entity[':track/owner_id'],
-    url: entity[':track/url'],
-    index: entity[':track/vk-index']
-  });
-};
 
 Track.prototype.toString = function() {
   return 'Track #' + this.id;
@@ -44,20 +28,6 @@ Track.prototype.equals = function(other) {
 
 Track.prototype.modify = function (attrs) {
   return new Track(_.extend({}, this, attrs));
-};
-
-Track.prototype.toDatoms = function () {
-  var id = 'tracks/' + this.id;
-
-  return [
-    [id, ':track/title', this.title],
-    [id, ':track/artist', this.artist],
-    [id, ':track/duration', this.duration],
-    [id, ':track/owner_id', this.owner_id],
-    [id, ':track/url', this.url],
-    [id, ':track/vkid', this.id],
-    [id, ':track/vk-index', this.index]
-  ];
 };
 
 module.exports = Track;
