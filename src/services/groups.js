95c7106f0d7dc79644723c86db2d274219c19060
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2014-12-18 22:36:00 +0200

    Move vk loading logic to corresponding service

--- src/services/groups.js
+++ src/services/groups.js
@@ -1,45 +1,7 @@
-var vk = require('app/vk');
-var merge = require('app/utils').merge;
-var update = require('app/core/appstate').update;
 var groups = require('app/values/groups');
 var newsfeed = require('app/values/newsfeed');
 
-function loadGroups(user, offset, count, callback) {
-  vk.groups.get({
-    user_id: user.id,
-    offset: offset,
-    count: count,
-    extended: 1
-  }, function(err, res) {
-    if (err) {
-      return callback(err);
-    }
-
-    callback(null, merge(res.response, {
-      offset: offset
-    }));
-
-    if (res.response.count > 0 && res.response.count > offset + count) {
-      return loadGroups(user, offset + count, count, callback);
-    }
-  });
-}
-
 module.exports = function (receive, send) {
-  receive(':app/user', function(appstate, user) {
-    if (user.isAuthenticated()) {
-      loadGroups(user, 0, 1000, function(err, chunk) {
-        if (err) {
-          return console.log(err);
-        }
-
-        var newGroups = groups.fromVkResponse(chunk);
-
-        send({ e: 'app', a: ':app/groups', v: appstate.get('groups').merge(newGroups) });
-      });
-    }
-  });
-
   receive(':app/groups', function(appstate, v) {
     return appstate.set('groups', v);
   });
--- src/vk/service.js
+++ src/vk/service.js
@@ -1,11 +1,49 @@
 var _ = require('underscore');
+var groups = require('app/values/groups');
+var merge = require('app/utils').merge;
 var vk = require('./vk-api');
+
+
+function loadGroups(user, offset, count, callback) {
+  vk.groups.get({
+    user_id: user.id,
+    offset: offset,
+    count: count,
+    extended: 1
+  }, function(err, res) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, merge(res.response, {
+      offset: offset
+    }));
+
+    if (res.response.count > 0 && res.response.count > offset + count) {
+      return loadGroups(user, offset + count, count, callback);
+    }
+  });
+}
 
 module.exports = function VkService(receive, send, watch, mount) {
   mount(vk);
 
   receive(':app/user', function(appstate, user) {
     vk.authorize(user);
+  });
+
+  receive(':app/user', function(appstate, user) {
+    if (user.isAuthenticated()) {
+      loadGroups(user, 0, 1000, function(err, chunk) {
+        if (err) {
+          return console.log(err);
+        }
+
+        var newGroups = groups.fromVkResponse(chunk);
+
+        send({ e: 'app', a: ':app/groups', v: appstate.get('groups').merge(newGroups) });
+      });
+    }
   });
 
   receive(':vk/wall-request', function(appstate, request) {
