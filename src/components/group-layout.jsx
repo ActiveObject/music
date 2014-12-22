2b2b6a63957c568e7a297adac64521adb5ed5554
Vasyl Pryimachuk <vasyl.priymachuk@sethq.com>
2014-12-22 18:11:55 +0200

    Implement group layout as react component

---
+++ src/components/group-layout.jsx
@@ -0,0 +1,46 @@
+var React = require('react');
+var App = require('app/components/app.jsx');
+var GroupProfile = require('app/components/group-profile.jsx');
+var Newsfeed = require('app/components/newsfeed');
+var Player = require('app/components/player');
+var IScrollLayer = require('app/components/iscroll-layer.jsx');
+var Box = require('app/components/box.jsx');
+
+var appstate = require('app/core/appstate');
+
+var GroupLayout = React.createClass({
+  componentWillMount: function() {
+    this.group = appstate.groupById(this.props.id);
+    this.newsfeed = appstate.newsfeedForGroup(this.props.id);
+    this.activity = appstate.activityForGroup(this.props.id);
+  },
+
+  componentWillUnmount: function() {
+    // this.group.release();
+    // this.activity.release();
+    // this.newsfeed.release();
+  },
+
+  render: function() {
+    debugger
+    return (
+      <App layout={['two-region', 'group-layout']}>
+        <Box prefix='ra-' key='region-a'>
+          <GroupProfile group={this.group.atom.value} activity={this.activity.atom.value}></GroupProfile>
+        </Box>
+
+        <Box prefix='rb-' key='region-b'>
+          <IScrollLayer>
+            <Newsfeed newsfeed={this.newsfeed.atom.value} player={this.props.player}></Newsfeed>
+          </IScrollLayer>
+        </Box>
+
+        <Box prefix='rc-' key='region-c'>
+          <Player player={this.props.player}></Player>
+        </Box>
+      </App>
+    );
+  }
+});
+
+module.exports = GroupLayout;
--- src/layout/group-layout.js
+++ src/layout/group-layout.js
@@ -1,43 +1,16 @@
 var React = require('react');
-var App = React.createFactory(require('app/components/app.jsx'));
-var GroupProfile = React.createFactory(require('app/components/group-profile.jsx'));
-var Newsfeed = React.createFactory(require('app/components/newsfeed'));
-var Player = React.createFactory(require('app/components/player'));
-var IScrollLayer = React.createFactory(require('app/components/iscroll-layer.jsx'));
-var Box = React.createFactory(require('app/components/box.jsx'));
+var GroupLayoutCmp = React.createFactory(require('app/components/group-layout.jsx'));
 
 var MainLayout = require('app/layout/main-layout');
 var ArtistLayout = require('app/layout/artist-layout');
 var AuthLayout = require('app/layout/auth-layout');
 
-var appstate = require('app/core/appstate');
-
-
 function GroupLayout(attrs) {
   this.id = parseInt(attrs.id);
-  this.group = appstate.groupById(this.id);
-  this.newsfeed = appstate.newsfeedForGroup(this.id);
-  this.activity = appstate.activityForGroup(this.id);
 }
 
 GroupLayout.prototype.render = function (appstate) {
-  var profile = new GroupProfile({
-    group: this.group.atom.value,
-    activity: this.activity.atom.value
-  });
-
-  var newsfeed = new Newsfeed({
-    newsfeed: this.newsfeed.atom.value,
-    player: appstate.get('player')
-  });
-
-  var player = new Player({ player: appstate.get('player') });
-
-  var regionA = new Box({ prefix: 'ra-', key: 'region-a' }, profile);
-  var regionB = new Box({ prefix: 'rb-', key: 'region-b' }, new IScrollLayer(null, newsfeed));
-  var regionC = new Box({ prefix: 'rc-', key: 'region-c' }, player);
-
-  return new App({ layout: ['two-region', 'group-layout'] }, [regionA, regionB, regionC]);
+  return new GroupLayoutCmp({ id: this.id, player: appstate.get('player') });
 };
 
 GroupLayout.prototype.main = function () {
