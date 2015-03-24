c9f3098072db5b61525316f2ba0c064c4812e8e0
Vasyl Pryimachuk <vasyl.priymachuk@sethq.com>
2015-03-24 14:36:32 +0200

    Added 'next n' button to tracklist

--- src/components/tracklist.jsx
+++ src/components/tracklist.jsx
@@ -10,8 +10,15 @@ var Tracklist = React.createClass({
     tracklist: React.PropTypes.object.isRequired
   },
 
+  getInitialState: function() {
+    return {
+      count: 5,
+      step: 5
+    };
+  },
+
   render: function() {
-    var tracks = this.props.tracklist.playlist.tracks.slice(0, 5).map(function (track) {
+    var tracks = this.props.tracklist.playlist.tracks.slice(0, this.state.count).map(function (track) {
       var isActive = track.id === this.props.player.track.id;
       var isPlaying = isActive && this.props.player.isPlaying;
 
@@ -29,12 +36,20 @@ var Tracklist = React.createClass({
     return (
       <div className='tracklist'>
         <div className='tracklist-body'>{tracks}</div>
+        <div className='tracklist-more' onClick={this.next}>next {this.state.step}</div>
       </div>
     );
   },
 
   togglePlay: function (track) {
     vbus.push(this.props.player.togglePlay(track, this.props.tracklist));
+  },
+
+  next: function() {
+    this.setState({
+      count: this.state.count + this.state.step,
+      step: this.state.count > 5 ? 10 : this.state.count
+    });
   }
 });
 
