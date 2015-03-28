a1ef369b879203f4ca6260fa0c4fa442390682a0
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-03-28 14:10:26 +0200

    Using jsx for auth component

---
+++ src/components/auth.jsx
@@ -0,0 +1,22 @@
+require('app/styles/auth.styl');
+require('app/styles/element.styl');
+
+var React = require('react');
+var Icon = require('app/components/icon');
+
+var Auth = React.createClass({
+  render: function () {
+    return (
+      <div className='auth'>
+        <a className='element-link auth-link' href={this.props.url}>
+          <div className='auth-bg-circle'>
+            <Icon id='shape-vkcom' className='auth-vk-icon' />
+            <div className='auth-vk-desc'>{'login wiht vk'}</div>
+          </div>
+        </a>
+      </div>
+    );
+  }
+});
+
+module.exports = Auth;
