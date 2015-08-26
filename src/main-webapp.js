06229f57ee6cb44869f623809124b54e903d6800
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-08-26 16:26:34 +0300

    Extract webapp system into the own module

--- src/main-webapp.js
+++ src/main-webapp.js
@@ -1,85 +1,8 @@
-var querystring = require('querystring');
-var each = require('underscore').each;
 var Atom = require('app/core/atom');
 var router = require('app/core/router');
 var render = require('app/core/renderer')(document.getElementById('app'));
-var db = require('app/core/db3');
-var vbus = require('app/core/vbus');
-var onValue = require('app/fn/onValue');
-var seq = require('app/core/db/producers/seq');
 var app = require('app');
-var { IGetItem, ISetItem } = require('app/Storage');
-var { IHttpRequest } = require('app/Http');
-var jsonpRequest = require('jsonp');
-var AuthRoute = require('app/routes/auth-route');
-var vkAccount = require('app/values/accounts/vk');
-
-function System() {
-  this.uninstallList = [];
-}
-
-System.prototype[IGetItem] = function (key, fn) {
-  if (localStorage.hasOwnProperty(key)) {
-    fn(localStorage.getItem(key));
-  }
-};
-
-System.prototype[ISetItem] = function (item) {
-  each(item, (value, key) => localStorage.setItem(key, value));
-};
-
-System.prototype[IHttpRequest] = function (url, callback) {
-  return jsonpRequest(url, callback);
-};
-
-System.prototype.start = function () {
-  var hash = location.hash;
-  this.uninstallList.push(onValue(vbus.map(seq(0)), (produce) => db.modify(produce)));
-  this.uninstallList.push(require('app/services/vk-indexing-service')(vbus));
-  this.uninstallList.push(require('app/services/router-service')(vbus));
-  this.uninstallList.push(require('app/services/vk-service')(vbus));
-  this.uninstallList.push(require('app/services/soundmanager-service')(vbus));
-  this.uninstallList.push(require('app/services/local-storage-service')(vbus));
-  this.uninstallList.push(require('app/services/audio-key-control')(vbus));
-  this.auth(hash);
-};
-
-System.prototype.stop = function() {
-  this.uninstallList.forEach(uninstall => uninstall());
-};
-
-System.prototype.auth = function (hash) {
-  function hasToken(hash) {
-    return hash && querystring.parse(hash.slice(1)).access_token;
-  }
-
-  function isUserInStorage() {
-    return localStorage.getItem('user_id') && localStorage.getItem('access_token');
-  }
-
-  if (hasToken(hash)) {
-    var credentials = querystring.parse(hash.slice(1));
-
-    localStorage.setItem('user_id', credentials.user_id);
-    localStorage.setItem('access_token', credentials.access_token);
-
-    return vbus.emit({
-      tag: [':app/user', ':user/authenticated'],
-      id: credentials.user_id,
-      accessToken: credentials.access_token
-    });
-  }
-
-  if (isUserInStorage()) {
-    return vbus.emit({
-      tag: [':app/user', ':user/authenticated'],
-      id: localStorage.getItem('user_id'),
-      accessToken: localStorage.getItem('access_token')
-    });
-  }
-
-  return vbus.emit(new AuthRoute({ vkAccount: vkAccount }))
-};
+var WebappSystem = require('app/WebappSystem');
 
 Atom.listen(router, render);
 
@@ -96,5 +19,5 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app
-  .use(new System())
+  .use(new WebappSystem())
   .start();
