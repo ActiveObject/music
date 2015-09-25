fb9298a3ac55cbf725da756f121549fb198d3567
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-09-25 13:11:01 +0300

    Removed previous main layout components

--- src/ui/main-layout.jsx
+++ src/ui/main-layout.jsx
@@ -1,70 +1,12 @@
 var React = require('react/addons');
-var Immutable = require('immutable');
-var Atom = require('app/core/atom');
-var addToSet = require('app/fn/addToSet');
-var isValue = require('app/fn/isValue');
-var onValue = require('app/fn/onValue');
-
 var App = require('app/ui/app');
-var Box = require('app/ui/box');
-var IScrollLayer = require('app/ui/iscroll-layer');
-var GroupActivityCard = require('app/ui/group-activity-card');
-var PlayerContainer = require('app/ui/player-container');
-var UserPlaylists = require('app/ui/user-playlists');
 var db = require('app/db');
-
-var groups = require('app/db/groups');
-
-var ActivityList = React.createClass({
-  mixins: [React.addons.PureRenderMixin],
-
-  getInitialState: function () {
-    return {
-      groups: Atom.value(groups)
-    };
-  },
-
-  componentWillMount: function () {
-    this.unsub = Atom.listen(groups, v => this.setState({ groups: v }));
-  },
-
-  componentWillUnmount: function () {
-    this.unsub();
-  },
-
-  render: function () {
-    var cards = this.props.visibleGroups
-      .map(id => this.state.groups.find(group => group.id === id))
-      .filter(isValue)
-      .map(group => <GroupActivityCard period={this.props.period} group={group} />);
-
-    return <div>{cards}</div>;
-  }
-});
+var onValue = require('app/fn/onValue');
 
 var MainLayout = React.createClass({
   render: function() {
-    var visibleGroups = db.value.get(':db/visibleGroups');
-
     return (
-      <App layout={['two-region', 'main-layout']}>
-        <Box prefix='ra-' key='region-a'>
-          <IScrollLayer>
-            <span className='main-section-title'>Спільноти</span>
-            <ActivityList visibleGroups={visibleGroups} period={this.props.period} />
-          </IScrollLayer>
-        </Box>
-
-        <Box prefix='rb-' key='region-b'>
-          <IScrollLayer>
-            <UserPlaylists />
-          </IScrollLayer>
-        </Box>
-
-        <Box prefix='rc-' key='region-c'>
-          <PlayerContainer />
-        </Box>
-      </App>
+      <App>Hello world!</App>
     );
   }
 });
@@ -86,4 +28,4 @@ function updateOn(ComposedComponent, dbKey) {
   });
 }
 
-module.exports = updateOn(MainLayout, ':db/visibleGroups');
+module.exports = updateOn(MainLayout, ':db/tracks');
