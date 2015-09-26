af74a4de3f09a1cee4e84102c6bf0a952521e7d4
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-09-26 14:57:35 +0300

    Add command palette completion prototype

--- src/ui/CommandPalette.jsx
+++ src/ui/CommandPalette.jsx
@@ -11,6 +11,10 @@ export default React.createClass({
           onFocus={this.props.onFocus}
           onBlur={this.props.onBlur}
           onChange={(e) => this.props.onChange(e.target.value)} />
+        <div className='command-palette__complete'>
+          <span>All tracks</span>
+          <span className='command-palette__todo'>{' #breaks'}</span>
+        </div>
       </div>
     );
   }
