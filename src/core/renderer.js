09a19c9cb17bb1d6d3d86c20144fc545c735436b
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-08 16:29:54 +0300

    Renderer is no longer needed to track document visibility

--- src/core/renderer.js
+++ src/core/renderer.js
@@ -2,8 +2,6 @@ var ReactDOM = require('react-dom');
 
 module.exports = function render(mountNode) {
   return function renderRoot(root) {
-    if (!document.hidden) {
-      ReactDOM.render(root, mountNode);
-    }
+    ReactDOM.render(root, mountNode);
   };
 };
