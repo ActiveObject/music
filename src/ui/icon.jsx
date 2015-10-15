e7a437309cfcc5a1516b935408babd99324ee8fe
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-15 16:04:17 +0300

    Change icon to be stateful functional component

--- src/ui/icon.jsx
+++ src/ui/icon.jsx
@@ -1,13 +1,7 @@
-var React = require('react');
-
-var Icon = React.createClass({
-  render: function () {
-    return (
-      <svg className='icon' viewBox='0 0 100 100'>
-        <use xlinkHref={`#${this.props.id}`} />
-      </svg>
-    );
-  }
-});
-
-module.exports = Icon;
+export default function Icon({ id }) {
+  return (
+    <svg className='icon' viewBox='0 0 100 100'>
+      <use xlinkHref={`#${id}`} />
+    </svg>
+  );
+}
