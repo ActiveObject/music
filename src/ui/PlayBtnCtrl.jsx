576f3d4240b91649b8d46b3b43bf0931fa1493c6
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-18 11:35:15 +0300

    PlayBtnCtrl is now a stateless functional component

--- src/ui/PlayBtnCtrl.jsx
+++ src/ui/PlayBtnCtrl.jsx
@@ -6,16 +6,13 @@ import updateOnKey from 'app/fn/updateOnKey';
 import * as Player from 'app/values/player';
 import PlayBtn from 'app/ui/PlayBtn';
 
-var PlayBtnCtrl = React.createClass({
-  render: function () {
-    var isPlaying = hasTag(db.value.get(':db/player'), ':player/is-playing');
+function PlayBtnCtrl() {
+  var isPlaying = hasTag(db.value.get(':db/player'), ':player/is-playing');
+  return <PlayBtn isPlaying={isPlaying} onClick={togglePlay} />;
+}
 
-    return <PlayBtn isPlaying={isPlaying} onClick={this.togglePlay} />;
-  },
-
-  togglePlay: function () {
-    vbus.emit(Player.togglePlay(db.value.get(':db/player')));
-  }
-})
+function togglePlay() {
+  vbus.emit(Player.togglePlay(db.value.get(':db/player')));
+}
 
 export default updateOnKey(PlayBtnCtrl, ':db/player');
