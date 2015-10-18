a293ed79c4c416570a17341f7ec5a9e00e612737
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-18 11:09:39 +0300

    PlaylistUI is now a stateless functional component

--- src/ui/PlaylistUI.jsx
+++ src/ui/PlaylistUI.jsx
@@ -7,61 +7,58 @@ import updateOnKey from 'app/fn/updateOnKey';
 import IScrollLayer from 'app/ui/iscroll-layer';
 import LazyTracklist from 'app/ui/LazyTracklist';
 
-var PlaylistUI = updateOnKey(React.createClass({
-  render: function () {
-    var ctx = db.value.get(':db/context');
-    var isCmdActivated = hasTag(db.value.get(':db/command-palette'), ':cmd/is-activated');
-    var tracks = this.tracks(ctx);
+function PlaylistUI({ tracks }) {
+  var ctx = db.value.get(':db/context');
+  var isCmdActivated = hasTag(db.value.get(':db/command-palette'), ':cmd/is-activated');
 
-    return (
-      <Motion
-        defaultStyle={{
-          zoom: 100,
-          y: 0,
-          opacity: 100
-        }}
+  return (
+    <Motion
+      defaultStyle={{
+        zoom: 100,
+        y: 0,
+        opacity: 100
+      }}
 
-        style={{
-          zoom: spring(isCmdActivated ? 80 : 100),
-          y: spring(isCmdActivated ? 200 : 0),
-          opacity: spring(isCmdActivated ? 0 : 100)
-        }}>
-        {interpolated =>
-          <div className='playlist' style={{ transform: `scale(${interpolated.zoom / 100}) translate(0, ${interpolated.y}px)`, opacity: `${interpolated.opacity / 100}` }}>
-            <div className='playlist__content'>
-              <div className='playlist__columns'>
-                <div className='track__index'>#</div>
-                <div className='track__artist'>artist</div>
-                <div className='track__title'>track</div>
-                <div className='track__duration'>time</div>
-              </div>
-              <div className='playlist__table'>
-                <LazyTracklist tracks={tracks} />
-              </div>
+      style={{
+        zoom: spring(isCmdActivated ? 80 : 100),
+        y: spring(isCmdActivated ? 200 : 0),
+        opacity: spring(isCmdActivated ? 0 : 100)
+      }}>
+      {interpolated =>
+        <div className='playlist' style={{ transform: `scale(${interpolated.zoom / 100}) translate(0, ${interpolated.y}px)`, opacity: `${interpolated.opacity / 100}` }}>
+          <div className='playlist__content'>
+            <div className='playlist__columns'>
+              <div className='track__index'>#</div>
+              <div className='track__artist'>artist</div>
+              <div className='track__title'>track</div>
+              <div className='track__duration'>time</div>
             </div>
-            <div className='playlist__paginator'></div>
+            <div className='playlist__table'>
+              <LazyTracklist tracks={filterTracksWithGivenCtx(tracks, ctx)} />
+            </div>
           </div>
-        }
-      </Motion>
-    )
-  },
+          <div className='playlist__paginator'></div>
+        </div>
+      }
+    </Motion>
+  );
+}
 
-  tracks: function (ctx) {
-    if (hasTag(ctx, ':context/filter-by-artist')) {
-      return this.props.tracks.filter(matchByArtist(ctx.filter.value));
-    }
-
-    if (hasTag(ctx, ':context/filter-by-tag')) {
-      return this.props.tracks.filter(matchByTag(ctx.filter.value));
-    }
-
-    if (hasTag(ctx, ':context/filter-by-track')) {
-      return this.props.tracks.filter(matchByTrackTitle(ctx.filter.value));
-    }
-
-    return this.props.tracks;
+function filterTracksWithGivenCtx(tracks, ctx) {
+  if (hasTag(ctx, ':context/filter-by-artist')) {
+    return tracks.filter(matchByArtist(ctx.filter.value));
   }
-}), [':db/context', ':db/command-palette']);
+
+  if (hasTag(ctx, ':context/filter-by-tag')) {
+    return tracks.filter(matchByTag(ctx.filter.value));
+  }
+
+  if (hasTag(ctx, ':context/filter-by-track')) {
+    return tracks.filter(matchByTrackTitle(ctx.filter.value));
+  }
+
+  return tracks;
+}
 
 function matchByArtist(artist) {
   return function match(track) {
@@ -83,5 +80,4 @@ function matchByTrackTitle(title) {
   };
 }
 
-
-export default PlaylistUI;
+export default updateOnKey(PlaylistUI, [':db/context', ':db/command-palette']);
