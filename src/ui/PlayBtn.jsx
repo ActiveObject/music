250446d9d3aae1f8b7797fe5538c2ecff0a2b8fa
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-18 11:31:35 +0300

    PlayBtn is now a stateless functional component

--- src/ui/PlayBtn.jsx
+++ src/ui/PlayBtn.jsx
@@ -4,25 +4,21 @@ import Icon from 'app/ui/icon';
 import cx from 'classnames';
 import { hasTag } from 'app/Tag';
 
-var PlayBtn = React.createClass({
-  render: function () {
-    var classes = cx({
-      'play-btn': true,
-      'play-btn--active': this.props.isPlaying
-    });
+export default function PlayBtn({ isPlaying, onClick }) {
+  var classes = cx({
+    'play-btn': true,
+    'play-btn--active': isPlaying
+  });
 
-    return (
-      <Motion
-        defaultStyle={{ rotate: 0 }}
-        style={{ rotate: spring(this.props.isPlaying ? 90 : 0, [160, 17]) }}>
-        {interpolated =>
-          <div key='play-btn' className={classes} onClick={this.props.onClick} style={{ transform: `rotate(${interpolated.rotate}deg)` }}>
-            <Icon id={this.props.isPlaying ? 'shape-pause' : 'shape-iconmonstr-arrow-37-icon'} />
-          </div>
-        }
-      </Motion>
-    );
-  }
-});
-
-export default PlayBtn;
+  return (
+    <Motion
+      defaultStyle={{ rotate: 0 }}
+      style={{ rotate: spring(isPlaying ? 90 : 0, [160, 17]) }}>
+      {interpolated =>
+        <div key='play-btn' className={classes} onClick={onClick} style={{ transform: `rotate(${interpolated.rotate}deg)` }}>
+          <Icon id={isPlaying ? 'shape-pause' : 'shape-iconmonstr-arrow-37-icon'} />
+        </div>
+      }
+    </Motion>
+  );
+}
