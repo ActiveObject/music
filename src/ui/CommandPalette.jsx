b6a653e2486f2cc7ea991cc248facc0ac1e47750
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-18 11:03:11 +0300

    Using class syntax for CommandPalette component

--- src/ui/CommandPalette.jsx
+++ src/ui/CommandPalette.jsx
@@ -6,16 +6,16 @@ import vbus from 'app/core/vbus';
 import { hasTag } from 'app/Tag';
 import updateOnKey from 'app/fn/updateOnKey';
 
-var CommandPalette = React.createClass({
-  componentDidUpdate: function () {
+class CommandPalette extends React.Component {
+  componentDidUpdate() {
     var isActivated = hasTag(db.value.get(':db/command-palette'), ':cmd/is-activated');
 
     if (isActivated && document.activeElement !== this._input) {
       this._input.select();
     }
-  },
+  }
 
-  render: function () {
+  render() {
     var cmd = db.value.get(':db/cmd');
     var isActivated = hasTag(db.value.get(':db/command-palette'), ':cmd/is-activated');
 
@@ -37,6 +37,7 @@ var CommandPalette = React.createClass({
               value={cmd}
               onFocus={this.activate}
               onBlur={this.deactivate}
+              onKeyUp={this.onKeyUp}
               onChange={throttle((e) => this.executeCommand(e.target.value), 200)} />
             <div className='command-palette__complete'>
               <span>All tracks</span>
@@ -46,26 +47,36 @@ var CommandPalette = React.createClass({
         }
       </Motion>
     );
-  },
+  }
 
-  executeCommand: function (cmd) {
+  onKeyUp(e) {
+    if (e.keyCode === 27) {
+      vbus.emit({
+        tag: [':app/command-palette']
+      });
+    }
+
+    console.log(e.keyCode)
+  }
+
+  executeCommand(cmd) {
     vbus.emit({
       tag: ':app/cmd',
       value: cmd
     });
-  },
+  }
 
-  activate: function () {
+  activate() {
     vbus.emit({
       tag: [':app/command-palette', ':cmd/is-activated']
     });
-  },
+  }
 
-  deactivate: function () {
+  deactivate() {
     vbus.emit({
       tag: [':app/command-palette']
     });
   }
-});
+}
 
 export default updateOnKey(CommandPalette, [':db/command-palette', ':db/cmd']);
