0cdccb6aec1af87d176700e80eed45318ec93c9f
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-23 12:36:07 +0300

    Fix auth view styles

--- src/auth/AuthView.jsx
+++ src/auth/AuthView.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Icon from 'app/ui/icon';
 
+import './auth.css';
+
 function AuthView({ url }) {
   return (
     <div className='auth'>
       <a className='element-link auth-link' href={url}>
         <div className='auth-bg-circle'>
           <Icon id='shape-vkcom' className='auth-vk-icon' />
-          <div className='auth-vk-desc'>{'login wiht vk'}</div>
+          <div className='auth-vk-desc'>{'login with vk'}</div>
         </div>
       </a>
     </div>
