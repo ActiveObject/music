7ebd2956678170e044be0e7e35e8c5deabab6192
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-24 19:55:06 +0300

    Using db instead vbus to track user changes in vk component

--- src/vk/driver.js
+++ src/vk/driver.js
@@ -1,7 +1,14 @@
-import { hasTag } from 'app/Tag';
-import onValue from 'app/onValue';
+import Atom from 'app/Atom';
+import db from 'app/db';
 import vk from 'app/vk';
+import { hasTag } from 'app/Tag';
 
 export default function (vbus) {
-  return onValue(vbus.filter(v => hasTag(v, ':user/authenticated')), user => vk.authorize(user));
+  return Atom.listen(db, function (dbVal) {
+    var user = dbVal.get(':db/user');
+
+    if (hasTag(user, ':user/authenticated')) {
+      vk.authorize(user);
+    }
+  });
 }
