d15ca59c1dfc994101b8345e2291ecc59bca32f5
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-10-26 12:48:02 +0200

    Collect every key binding for later unbinding

--- src/key-control-driver.js
+++ src/key-control-driver.js
@@ -3,27 +3,35 @@ import db from 'app/db';
 import * as Player from 'app/Player';
 import { toggleTag } from 'app/Tag';
 
-export default function (vbus) {
-  key('left', function () {
-    vbus.push(Player.rewind(db.value.get(':db/player'), 5000));
-  });
+function bindKeys(fn) {
+  var keyCombinations = [];
 
-  key('right', function () {
-    vbus.push(Player.forward(db.value.get(':db/player'), 5000));
-  });
-
-  key('space', function () {
-    vbus.push(Player.togglePlay(db.value.get(':db/player')));
-  });
-
-  key('command+shift+p', function () {
-    vbus.push(toggleTag(db.value.get(':db/command-palette'), ':cmd/is-activated'));
+  fn(function (...args) {
+    keyCombinations.push(args[0]);
+    key(...args);
   });
 
   return function () {
-    key.unbind('left');
-    key.unbind('right');
-    key.unbind('space');
-    key.unbind('command+shift+p');
+    keyCombinations.forEach(x => key.unbind(x));
   };
+}
+
+export default function (vbus) {
+  return bindKeys(function (key) {
+    key('left', function () {
+      vbus.push(Player.rewind(db.value.get(':db/player'), 5000));
+    });
+
+    key('right', function () {
+      vbus.push(Player.forward(db.value.get(':db/player'), 5000));
+    });
+
+    key('space', function () {
+      vbus.push(Player.togglePlay(db.value.get(':db/player')));
+    });
+
+    key('command+shift+p', function () {
+      vbus.push(toggleTag(db.value.get(':db/command-palette'), ':cmd/is-activated'));
+    });
+  });
 }
