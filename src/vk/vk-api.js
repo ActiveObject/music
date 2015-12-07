319e9f5925050b61a0b1334663e4fd089a8fc049
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-12-07 21:08:30 +0200

    Remove custom vk api states, using plain js object instead

--- src/vk/vk-api.js
+++ src/vk/vk-api.js
@@ -1,53 +1,19 @@
-import Atom from 'app/Atom';
 import merge from 'app/merge';
 import Request from './request';
 import Response from './response';
 import setupHelpers from './setupHelpers';
 
-function UnathorizedApiState() {
-  this.isAuthorized = false;
-}
-
-UnathorizedApiState.prototype.authorize = function (user) {
-  return new ProcessingApiState({
-    user: user.id,
-    token: user.accessToken
-  });
-};
-
-UnathorizedApiState.prototype.process = function (queue) {
-  return queue;
-};
-
-function ProcessingApiState(attrs) {
-  this.token = attrs.token;
-  this.user = attrs.user;
-  this.isAuthorized = true;
-}
-
-ProcessingApiState.prototype.authorize = function (user) {
-  return this;
-};
-
-ProcessingApiState.prototype.process = function (reqParams, callback) {
-  var req = new Request(merge(reqParams, {
-    token: this.token
-  }));
-
-  req.send((err, data) => callback(err, data, req));
-};
-
 function VkApi(attrs) {
   this.interval = 1000 / attrs.rateLimit;
-  this.atom = attrs.atom;
   this.version = attrs.version;
   this.entryPoint = attrs.entryPoint;
+  this.state = { isAuthorized: false };
   this.queue = [];
   this.start();
   setupHelpers(this);
 }
 
-VkApi.prototype.start = function() {
+VkApi.prototype.start = function () {
   var nextTick = () => {
     this.timer = setTimeout(() => {
       this.process();
@@ -64,8 +30,12 @@ VkApi.prototype.stop = function () {
   }
 };
 
-VkApi.prototype.authorize = function(user) {
-  Atom.update(this, state => state.authorize(user));
+VkApi.prototype.authorize = function (user) {
+  this.state = {
+    isAuthorized: true,
+    user: user.id,
+    token: user.accessToken
+  };
 };
 
 VkApi.prototype.request = function (method, options, done) {
@@ -79,9 +49,13 @@ VkApi.prototype.request = function (method, options, done) {
   });
 };
 
-VkApi.prototype.process = function() {
-  if (this.queue.length > 0) {
-    this.atom.value.process(this.queue[0], (err, data, req) => {
+VkApi.prototype.process = function () {
+  if (this.state.isAuthorized && this.queue.length > 0) {
+    var req = new Request(merge(this.queue[0], {
+      token: this.token
+    }));
+
+    req.send((err, data) => {
       var res = new Response(err, data);
 
       if (res.tooManyRequests() || res.captchaNeeded()) {
@@ -94,9 +68,8 @@ VkApi.prototype.process = function() {
   }
 };
 
-module.exports = new VkApi({
+export default new VkApi({
   rateLimit: 2,
   version: '5.29',
-  entryPoint: 'https://api.vk.com/method/',
-  atom: new Atom(new UnathorizedApiState())
+  entryPoint: 'https://api.vk.com/method/'
 });
