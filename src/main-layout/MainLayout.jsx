920772b0d79097ecc86584aaace554338219462c
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-01-23 14:27:06 +0200

    MainLayout as stateless functional component

--- src/main-layout/MainLayout.jsx
+++ src/main-layout/MainLayout.jsx
@@ -11,68 +11,66 @@ import PlayBtnCtrl from './PlayBtnCtrl';
 import Profile from './Profile';
 import CmdOut from './CmdOut';
 
-class MainLayout extends React.Component {
-  render() {
-    var isCmdActivated = hasTag(app.value.get(':db/command-palette'), ':cmd/is-activated');
+const MainLayout = () => {
+  var isCmdActivated = hasTag(app.value.get(':db/command-palette'), ':cmd/is-activated');
 
-    return (
-      <Layer>
-        <Layer className='app-container' style={{ padding: '70px 0' }}>
-          <Motion
-            defaultStyle={{
-              zoom: 100,
-              y: 0,
-              opacity: 100
-            }}
-
-            style={{
-              zoom: spring(isCmdActivated ? 80 : 100),
-              y: spring(isCmdActivated ? 200 : 0),
-              opacity: spring(isCmdActivated ? 0 : 100)
-            }}>
-            {interpolated =>
-              <div className='main-layout' style={{ transform: `scale(${interpolated.zoom / 100}) translate(0, ${interpolated.y}px)`, opacity: `${interpolated.opacity / 100}` }}>
-                <PlaylistCtrl />
-              </div>
-            }
-          </Motion>
-
-          <Motion
-            defaultStyle={{ opacity: 0 }}
-            style={{ opacity: spring(isCmdActivated ? 0 : 100) }} >
-            {interpolated =>
-              <div className='main-layout__play-btn' style={{ opacity: `${interpolated.opacity / 100}` }}>
-                <PlayBtnCtrl />
-              </div>
-            }
-          </Motion>
-
-          <div className='main-layout__profile'>
-            <Profile />
-          </div>
-        </Layer>
-
-        <Layer style={{ transform: `translate(0, ${isCmdActivated ? 0 : 100}%)` }}>
-          <CmdOut />
-        </Layer>
-
+  return (
+    <Layer>
+      <Layer className='app-container' style={{ padding: '70px 0' }}>
         <Motion
-          defaultStyle={{ fontSize: 2, y: 0 }}
-          style={{ fontSize: spring(isCmdActivated ? 3 : 2), y: spring(isCmdActivated ? 100 : 0) }}>
+          defaultStyle={{
+            zoom: 100,
+            y: 0,
+            opacity: 100
+          }}
+
+          style={{
+            zoom: spring(isCmdActivated ? 80 : 100),
+            y: spring(isCmdActivated ? 200 : 0),
+            opacity: spring(isCmdActivated ? 0 : 100)
+          }}>
           {interpolated =>
-            <div
-              className='app-cmd'
-              style={{
-                transform: `translate(0, ${interpolated.y}px)`,
-                fontSize: `${interpolated.fontSize}rem`
-              }}>
-              <CommandPalette />
+            <div className='main-layout' style={{ transform: `scale(${interpolated.zoom / 100}) translate(0, ${interpolated.y}px)`, opacity: `${interpolated.opacity / 100}` }}>
+              <PlaylistCtrl />
             </div>
           }
         </Motion>
+
+        <Motion
+          defaultStyle={{ opacity: 0 }}
+          style={{ opacity: spring(isCmdActivated ? 0 : 100) }} >
+          {interpolated =>
+            <div className='main-layout__play-btn' style={{ opacity: `${interpolated.opacity / 100}` }}>
+              <PlayBtnCtrl />
+            </div>
+          }
+        </Motion>
+
+        <div className='main-layout__profile'>
+          <Profile />
+        </div>
       </Layer>
-    );
-  }
+
+      <Layer style={{ transform: `translate(0, ${isCmdActivated ? 0 : 100}%)` }}>
+        <CmdOut />
+      </Layer>
+
+      <Motion
+        defaultStyle={{ fontSize: 2, y: 0 }}
+        style={{ fontSize: spring(isCmdActivated ? 3 : 2), y: spring(isCmdActivated ? 100 : 0) }}>
+        {interpolated =>
+          <div
+            className='app-cmd'
+            style={{
+              transform: `translate(0, ${interpolated.y}px)`,
+              fontSize: `${interpolated.fontSize}rem`
+            }}>
+            <CommandPalette />
+          </div>
+        }
+      </Motion>
+    </Layer>
+  );
 }
 
 export default updateOn(MainLayout, ':db/command-palette');
