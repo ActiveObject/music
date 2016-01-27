cf0ad6a0c5e11692e829fa257c2954cbbd80749b
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-01-27 19:17:50 +0200

    StaticTracklist: show all given tracks when limit is not set

--- src/tracklist/StaticTracklist.jsx
+++ src/tracklist/StaticTracklist.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TrackCtrl from 'app/tracklist/TrackCtrl';
 
-const StaticTracklist = ({ tracks, limit = 5 }) =>
+const StaticTracklist = ({ tracks, limit = tracks.count() }) =>
   <div>
     {tracks.slice(0, limit).map(t => <TrackCtrl key={t.id} track={t} tracklist={tracks} />)}
   </div>
