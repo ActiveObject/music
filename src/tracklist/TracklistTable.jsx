ba30de67546470c9fbb442676710ba890cdb9d48
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-02-25 15:53:58 +0200

    Change css class to TracklistTable

--- src/tracklist/TracklistTable.jsx
+++ src/tracklist/TracklistTable.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './TracklistTable.css';
 
 const TracklistTable = ({ children }) =>
-  <div className='playlist'>
-    <div className='playlist__content'>
-      <div className='playlist__columns'>
+  <div className='TracklistTable'>
+    <div className='TracklistTable__content'>
+      <div className='TracklistTable__columns'>
         <div className='track__index'>#</div>
         <div className='track__artist'>artist</div>
         <div className='track__title'>track</div>
         <div className='track__duration'>time</div>
       </div>
-      <div className='playlist__table'>
+      <div className='TracklistTable__table'>
         {children}
       </div>
     </div>
