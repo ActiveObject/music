c8719c8efe9718e6964579f5d1f0b1f51a4c2430
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-02-25 15:54:17 +0200

    Change css class to TracklistPreview

--- src/tracklist/TracklistPreview.jsx
+++ src/tracklist/TracklistPreview.jsx
@@ -8,16 +8,16 @@ let repeat = (n, fn) => Array(...Array(n)).map((_, i) => fn(i))
 let TrackPreview = () => (
   <div className='track'>
     <div className='track__index'>
-      <div className='preview-block' style={{ width: 10 }}></div>
+      <div className='TracklistPreview__block' style={{ width: 10 }}></div>
     </div>
     <div className='track__artist'>
-      <div className='preview-block' style={{ width: `${randomNum(50, 100)}%` }} />
+      <div className='TracklistPreview__block' style={{ width: `${randomNum(50, 100)}%` }} />
     </div>
     <div className='track__title'>
-      <div className='preview-block' style={{ width: `${randomNum(50, 100)}%` }} />
+      <div className='TracklistPreview__block' style={{ width: `${randomNum(50, 100)}%` }} />
     </div>
     <div className='track__duration'>
-      <div className='preview-block' style={{ width: 30 }}></div>
+      <div className='TracklistPreview__block' style={{ width: 30 }}></div>
     </div>
   </div>
 );
