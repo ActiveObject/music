5716a58b6dd41ea4db492e14a378ab44d2666bb2
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-05-19 14:58:57 +0300

    Re-arrange AppRoot dependencies

--- src/app-root/AppRoot.jsx
+++ src/app-root/AppRoot.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import Authenticated from 'app/auth/Authenticated';
-import Layer from 'app/shared/Layer';
-import VkAudioSync from './VkAudioSync';
-import VkGroupSync from './VkGroupSync';
-import VkDriver from './VkDriver';
-import Soundmanager from 'app/shared/soundmanager/Soundmanager';
-import PlayerSync from './PlayerSync';
-import KeyboardDriver from './KeyboardDriver';
 import Router from 'react-router/lib/Router';
 import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
 import browserHistory from 'react-router/lib/browserHistory';
+
+import Authenticated from 'app/auth/Authenticated';
 import GroupPage from 'app/group/GroupPage';
 import MainPage from 'app/main/MainPage';
 import LibraryPage from 'app/library/LibraryPage';
+
+import Layer from 'app/shared/Layer';
+import Soundmanager from 'app/shared/soundmanager/Soundmanager';
+import VkAudioSync from './VkAudioSync';
+import VkGroupSync from './VkGroupSync';
+import VkDriver from './VkDriver';
+import PlayerSync from './PlayerSync';
+import KeyboardDriver from './KeyboardDriver';
 
 import './styles/base.css';
 import './styles/theme.css';
