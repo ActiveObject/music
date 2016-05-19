b2c09b053cefc24c714038df203f5d1c9c04c6d8
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-05-19 15:04:24 +0300

    Move app/app-root to app/AppRoot

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 import AppHost from 'app/AppHost';
-import AppRoot from 'app/app-root/AppRoot';
+import AppRoot from 'app/AppRoot';
 import app from 'app';
 import vk from 'app/shared/vk';
 
