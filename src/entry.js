31ee243a332db9cd8b5826f3a48892177ef024ce
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-05-31 16:16:24 +0300

    Enable hmr using react-hot-loader v3

--- src/entry.js
+++ src/entry.js
@@ -1,13 +1,28 @@
+import 'react-hot-loader/patch';
 import { render } from 'react-dom';
 import AppHost from 'app/AppHost';
 import MusicApp from 'app/MusicApp';
 import app from 'app';
 import vk from 'app/shared/vk';
+import { AppContainer as HotLoader } from 'react-hot-loader';
 
 render(
-  <AppHost value={app}>
-    <MusicApp />
-  </AppHost>, document.querySelector('#app'));
+  <HotLoader>
+    <AppHost value={app}>
+      <MusicApp />
+    </AppHost>
+  </HotLoader>, document.querySelector('#app'));
+
+if (module.hot) {
+  module.hot.accept('app/MusicApp', () => {
+    render(
+      <HotLoader>
+        <AppHost value={app}>
+          <MusicApp />
+        </AppHost>
+      </HotLoader>, document.querySelector('#app'));
+  });
+}
 
 window._app = app;
 window._vk = vk;
