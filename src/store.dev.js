b8141d325a71c7e3d30f5f6734c0e2bc5a3aaa78
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-08-28 23:20:06 +0300

    Add persistent debug sessions

--- src/store.dev.js
+++ src/store.dev.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { persistState } from 'redux-devtools';
 import DevTools from 'app/DevTools';
 import reducers from 'app/reducers';
 import { createPlayer } from 'app/shared/Player';
 
-let store = createStore(reducers, createPlayer(), DevTools.instrument());
+let store = createStore(reducers, createPlayer(), compose(DevTools.instrument(), persistState(getDebugSessionKey())));
+
+function getDebugSessionKey() {
+  const matches = window.location.href.match(/[?&]debug_session=([^&#]+)\b/);
+  return (matches && matches.length > 0)? matches[1] : null;
+}
 
 if (module.hot) {
   module.hot.accept('app/reducers', () =>
