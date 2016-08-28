5eddbc08dcacacf188917efdc17eb19df80875d0
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-08-28 23:19:51 +0300

    Filter out actions

--- src/DevTools.js
+++ src/DevTools.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
 
 export default createDevTools(
   <DockMonitor toggleVisibilityKey='ctrl-h'
                changePositionKey='ctrl-q'
-               defaultIsVisible={true}>
-    <LogMonitor theme='tomorrow' />
+               defaultIsVisible={false}>
+    <FilterMonitor blacklist={['PLAYER_UPDATE_POSITION', 'PLAYER_UPDATE_LOADING']}>
+      <LogMonitor theme='tomorrow' />
+    </FilterMonitor>
   </DockMonitor>
 );
