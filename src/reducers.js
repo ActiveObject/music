0bcf6dd2d0d43907f969065d872620a49e708f47
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-08-29 08:03:25 +0300

    Move player reducer to app/shared/Player

--- src/reducers.js
+++ src/reducers.js
@@ -1,17 +1,1 @@
-import { rewind, forward, togglePlay, nextTrack, finishSeeking, useTrack } from 'app/shared/Player';
-
-export default function (state, action) {
-  switch (action.type) {
-    case 'PLAYER_FORWARD': return forward(state, action.ms);
-    case 'PLAYER_REWIND': return rewind(state, action.ms);
-    case 'PLAYER_TOGGLE_PLAY': return togglePlay(state);
-    case 'PLAYER_TOGGLE_TRACK': return togglePlay(state, action.track, action.tracklist);
-    case 'PLAYER_UPDATE_POSITION': return Object.assign({}, state, { position: action.position });
-    case 'PLAYER_UPDATE_LOADING': return Object.assign({}, state, { bytesLoaded: action.bytesLoaded, bytesTotal: action.bytesTotal });
-    case 'PLAYER_NEXT_TRACK': return nextTrack(state);
-    case 'PLAYER_FINISH_SEEKING': return finishSeeking(state);
-    case 'PLAYER_USE_TRACK': return useTrack(state, action.track);
-  }
-
-  return state;
-}
+export { reducer as default } from 'app/shared/Player';
--- src/shared/Player.js
+++ src/shared/Player.js
@@ -13,6 +13,22 @@ export function createPlayer() {
   };
 }
 
+export function reducer(state, action) {
+  switch (action.type) {
+    case 'PLAYER_FORWARD': return forward(state, action.ms);
+    case 'PLAYER_REWIND': return rewind(state, action.ms);
+    case 'PLAYER_TOGGLE_PLAY': return togglePlay(state);
+    case 'PLAYER_TOGGLE_TRACK': return togglePlay(state, action.track, action.tracklist);
+    case 'PLAYER_UPDATE_POSITION': return Object.assign({}, state, { position: action.position });
+    case 'PLAYER_UPDATE_LOADING': return Object.assign({}, state, { bytesLoaded: action.bytesLoaded, bytesTotal: action.bytesTotal });
+    case 'PLAYER_NEXT_TRACK': return nextTrack(state);
+    case 'PLAYER_FINISH_SEEKING': return finishSeeking(state);
+    case 'PLAYER_USE_TRACK': return useTrack(state, action.track);
+  }
+
+  return state;
+}
+
 export function play(p) {
   return addTag(p, ':player/is-playing');
 }
