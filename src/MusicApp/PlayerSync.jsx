ce95832b817de24cacc43e133fbcf809333e45dc
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-09-20 21:29:41 +0300

    Remove no longer used modules

--- src/MusicApp/PlayerSync.jsx
+++ src/MusicApp/PlayerSync.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateOn } from 'app/AppHost';
 import { useTrack } from 'app/playerActions';
 import { hasTag } from 'app/shared/Tag';
 
