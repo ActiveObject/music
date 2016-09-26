04f52bf18d1b74eaef5ce7a78b93b3d318128a71
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-09-26 21:53:56 +0300

    Render different shapes in Group

--- src/Group/Group.js
+++ src/Group/Group.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'react-router/Link';
 import MediaQuery from 'react-responsive';
 import GroupProfile from './GroupProfile';
 import GroupProfilePreview from './GroupProfilePreview';
@@ -7,7 +8,29 @@ import GroupActivity from './GroupActivity';
 import FetchGroup from './FetchGroup';
 import 'app/shared/ResponsiveGrid.css';
 
-const Group = ({ id }) =>
+let randomNum = (min, max) => min + Math.floor(Math.random() * (max - min));
+
+const GroupListItemPreview = () =>
+  <div className='track' style={{ height: 50 }}>
+    <div className='GroupsList__image'>
+      <div className='GroupsListPreview__image-block'></div>
+    </div>
+    <div className='GroupsList__title'>
+      <div className='TracklistPreview__block' style={{ width: `${randomNum(50, 100)}%` }}></div>
+    </div>
+  </div>
+
+const GroupListItem = ({ group }) =>
+  <div className='track' style={{ height: 50 }}>
+    <div className='GroupsList__image' style={{ width: 30, height: 30 }}>
+      <img src={group.photo_50} width={30} height={30} />
+    </div>
+    <div className='GroupsList__name'>
+      <Link to={`/groups/${group.id}`} data-track-key={group.id}>{group.name}</Link>
+    </div>
+  </div>
+
+const GroupFullscreen = ({ id }) =>
   <div className='Group'>
     <FetchGroup id={id}>
       {({ isLoading, group }) =>
@@ -28,5 +51,19 @@ const Group = ({ id }) =>
       <GroupTop5Tracks groupId={id} />
     </section>
   </div>
+
+const Group = ({ id, shape = 'fullscreen' }) => {
+  if (shape === 'list-item') {
+    return (
+      <FetchGroup id={id}>
+        {({ isLoading, group }) => isLoading ? <GroupListItemPreview /> : <GroupListItem group={group} /> }
+      </FetchGroup>
+    );
+  }
+
+  if (shape === 'fullscreen') {
+    return <GroupFullscreen id={id} />
+  }
+}
 
 export default Group;
