862d6c42a67042f3452da11e3dbb2ef8adacfaf2
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-10-29 19:22:59 +0300

    Move GroupSync to GroupsListContainer

--- src/Home/GroupsListContainer.js
+++ src/Home/GroupsListContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Map } from 'immutable';
-import GroupSync from './GroupSync';
+import { connect } from 'react-redux';
+import difference from 'lodash/difference';
+import vk from 'app/shared/vk';
 
 class GroupsListContainer extends React.Component {
   state = {
@@ -70,5 +72,48 @@ class Tracker extends React.Component {
     }
   }
 }
+
+class GroupSync extends React.Component {
+  componentWillMount() {
+    this.sync();
+  }
+
+  componentDidUpdate() {
+    this.sync();
+  }
+
+  render() {
+    return this.props.children;
+  }
+
+  sync() {
+    var idsInUse = this.props.groups;
+    var idsInCache = [...this.props.cache.keys()];
+
+    var outdated = difference(idsInCache, idsInUse);
+    var missing = difference(idsInUse, idsInCache);
+
+    console.log(`[GroupSync] ${missing.length} missing, ${outdated.length} outdated`);
+
+    if (missing.length > 0) {
+      vk.groups.getById({
+        group_ids: missing.slice(0, 100).join(',')
+      }, (err, res) => {
+        if (err) {
+          return console.log(err);
+        }
+
+        var cache = res.response.reduce((c, g) => c.set(String(g.id), g), this.props.cache);
+
+        this.props.onSync(cache);
+      });
+    }
+  }
+}
+
+GroupSync.propTypes = {
+  cache: React.PropTypes.object.isRequired,
+  onSync: React.PropTypes.func.isRequired
+};
 
 export default GroupsListContainer;
