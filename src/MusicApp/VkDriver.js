c99530dbff746eb15c11c6bdc6b574502066a24f
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-11-17 21:42:42 +0200

    Move request and response to VkDriver

--- src/MusicApp/VkDriver.js
+++ src/MusicApp/VkDriver.js
@@ -1,8 +1,8 @@
 import { EventEmitter } from 'events';
+import Url from 'url';
 import React from 'react';
+import request from 'jsonp';
 import merge from 'app/shared/merge';
-import Request from 'app/shared/vk/request';
-import Response from 'app/shared/vk/response';
 
 class VkDriver extends React.Component {
   state = {
@@ -138,5 +138,84 @@ class VkDriver extends React.Component {
     }
   }
 }
+
+function Request(attrs) {
+  this.entryPoint = attrs.entryPoint;
+  this.token = attrs.token;
+  this.callback = attrs.callback;
+  this.attempt = attrs.attempt;
+  this.method = attrs.method;
+  this.params = attrs.params;
+  this.version = attrs.version;
+
+  this.url = Url.format({
+    protocol: Url.parse(this.entryPoint).protocol,
+    host: Url.parse(this.entryPoint).host,
+    pathname: Url.parse(this.entryPoint).pathname + this.method,
+    query: merge(this.params, {
+      access_token: this.token,
+      v: this.version
+    })
+  });
+};
+
+Request.prototype.send = function (callback) {
+  request(this.url, function (err, data) {
+    if (err) {
+      return callback(err);
+    }
+
+
+    if (data.error) {
+      return callback(data.error);
+    }
+
+    if (!data.response) {
+      return callback(new Error('Missing response body'));
+    }
+
+    callback(null, data);
+  });
+};
+
+Request.prototype.nextAttempt = function() {
+  return this.modify({
+    attempt: this.attempt + 1
+  });
+};
+
+Request.prototype.modify = function(attrs) {
+  return new Request(merge(this, attrs));
+};
+
+function Response(err, data) {
+  this.err = err;
+  this.data = data;
+}
+
+Response.TOO_MANY_REQUESTS = 6;
+Response.CAPTCHA_NEEDED = 14;
+
+Response.prototype.send = function(callback) {
+  if (this.tooManyRequests() || this.captchaNeeded()) {
+    return callback(this.err);
+  }
+
+  if (this.err) {
+    callback(this.err);
+  } else {
+    callback(null, this.data);
+  }
+
+  return this;
+};
+
+Response.prototype.tooManyRequests = function() {
+  return this.err && this.err.error_code === Response.TOO_MANY_REQUESTS;
+};
+
+Response.prototype.captchaNeeded = function() {
+  return this.err && this.err.error_code === Response.CAPTCHA_NEEDED;
+};
 
 export default VkDriver;
