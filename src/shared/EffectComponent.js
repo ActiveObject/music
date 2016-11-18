8e94f521553156734a217276236b7614956ab28d
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2016-11-18 09:59:29 +0200

    Specify effect type in its creator

--- src/shared/EffectComponent.js
+++ src/shared/EffectComponent.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class EffectComponent extends React.Component {
-  perform(effect) {
+  perform({ type, detail }) {
     var node = ReactDOM.findDOMNode(this);
-    var event = new CustomEvent("vk-request", {
-      detail: effect,
+    var event = new CustomEvent(type, {
+      detail,
       bubbles: true,
       cancelable: true
     });
